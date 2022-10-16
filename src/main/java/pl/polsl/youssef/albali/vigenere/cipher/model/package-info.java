bb824292592b97fc7bd43679df1fda0a6d0ab9fc
unknown <yousalb891@student.polsl.pl>
2022-10-16 15:26:27 +0200

    final-2

---
+++ src/main/java/pl/polsl/youssef/albali/vigenere/cipher/model/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Contains two class: {@link pl.polsl.youssef.albali.vigenere.cipher.model.VigenereCipherModel} model of the application responsible of encrypting and decrypting using Vigenère cipher
+ * of keywords: {@code package} and {@code import} and {@link pl.polsl.youssef.albali.vigenere.cipher.model.InvalidCharacterException} exception used when an an invalid key or string was chosen
+ */
+
+package pl.polsl.youssef.albali.vigenere.cipher.model;
---
+++ src/main/java/pl/polsl/youssef/albali/vigenere/cipher/view/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Contains one class: {@link pl.polsl.youssef.albali.vigenere.cipher.view.VigenereCipherView} needed to handle inputs and outputs 
+ * of keywords: {@code package} and {@code import}
+ */
+package pl.polsl.youssef.albali.vigenere.cipher.view;
