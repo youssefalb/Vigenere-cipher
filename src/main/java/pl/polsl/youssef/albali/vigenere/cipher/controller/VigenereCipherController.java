a1c3074d1a961e58f1edd067d0b8a6c52bce9996
unknown <yousalb891@student.polsl.pl>
2022-11-12 21:55:13 +0100

    'deleted_useless_frame'

--- src/main/java/pl/polsl/youssef/albali/vigenere/cipher/controller/VigenereCipherController.java
+++ src/main/java/pl/polsl/youssef/albali/vigenere/cipher/controller/VigenereCipherController.java
@@ -4,8 +4,8 @@
  */
 package pl.polsl.youssef.albali.vigenere.cipher.controller;
 import java.util.*;
-import java.util.stream.*;
-import javax.swing.JFrame;
+import java.util.stream.Stream;
+import javax.swing.JOptionPane;
 import pl.polsl.youssef.albali.vigenere.cipher.view.*;
 import pl.polsl.youssef.albali.vigenere.cipher.model.*;
 
@@ -23,7 +23,8 @@ public class VigenereCipherController {
     /**
      * View instance used to get and output data
      */
-    private VigenereCipherView vigenereCipherView;
+    private VigenereCipherFrame vigenereGui;
+
     
     /**
      * Model instance used for encrypting and decrypting
@@ -37,23 +38,34 @@ public class VigenereCipherController {
     public static void main(String[] args) {
         List<String> argsList = Arrays.asList(args);
         VigenereCipherController vigenereCipherController = new VigenereCipherController();
-        vigenereCipherController.vigenereCipherView = new VigenereCipherView();
         vigenereCipherController.makeExamples(argsList);
-        VigenereCipherGui myGui = new VigenereCipherGui();
-        JFrame guiFrame = new JFrame();
-        guiFrame.setVisible(true);
-        guiFrame.add(myGui);
-        
-//        while(true){
-//            try{
-//                  vigenereCipherController.getOption();
-//            }
-//            catch(InvalidOptionException | InvalidCharacterException | EmptyKey e){
-//                vigenereCipherController.vigenereCipherView.printError( e.getMessage());
-//            }  
-//        }
+        vigenereCipherController.vigenereGui = new VigenereCipherFrame();
+        vigenereCipherController.initController();
+        vigenereCipherController.vigenereGui.setVisible(true);
+       
     }
-    
+    /**
+     * 
+     */
+    public void initController(){
+        vigenereGui.getEncryptButton().addActionListener(e->{
+            try {
+                encryptMessageAfterClick(vigenereGui.getEncryptionKey(),vigenereGui.getCurrentMessage());
+                vigenereGui.emptyEncryptionfFields();
+            } catch (InvalidCharacterException |EmptyKey ex) {
+                  JOptionPane.showMessageDialog(vigenereGui, ex.getMessage(), "Erorr", JOptionPane.WARNING_MESSAGE);
+            }
+        });
+        vigenereGui.getDecryptButton().addActionListener(e->{
+            try {
+                this.decryptMessageAfterClick(vigenereGui.getDecryptionKey(),vigenereGui.getCurrentCipherText());
+                vigenereGui.emptyDecryptionfFields();
+            } catch (InvalidCharacterException |EmptyKey ex) {
+                  JOptionPane.showMessageDialog(vigenereGui, ex.getMessage(), "Erorr", JOptionPane.WARNING_MESSAGE);
+            }
+        });
+           
+    }
     /**
      * Creates the examples and calls the view functions to print the tutorial to the user
      * @param args command line parameters used to display the encryption and decryption examples
@@ -61,84 +73,30 @@ public class VigenereCipherController {
      private void makeExamples(List<String> args){
         try{
             this.vigenereCipherModel = new VigenereCipherModel(args.get(2));
-            this.vigenereCipherView.printEncryptionTutorial(args.get(0), args.get(2),this.vigenereCipherModel.getEncryptedMessage(Stream.of(args.get(0))).get(0));
-            this.vigenereCipherView.printDecryptionTutorial(args.get(1), args.get(2),this.vigenereCipherModel.getDecryptedMessage(Stream.of(args.get(1))).get(0));
+            //this.vigenereCipherView.printEncryptionTutorial(args.get(0), args.get(2),this.vigenereCipherModel.getEncryptedMessage(Stream.of(args.get(0))).get(0));
+            //this.vigenereCipherView.printDecryptionTutorial(args.get(1), args.get(2),this.vigenereCipherModel.getDecryptedMessage(Stream.of(args.get(1))).get(0));
         }
         
         catch(InvalidCharacterException| EmptyKey e){
-                this.vigenereCipherView.printError( e.getMessage());
+              //  this.vigenereCipherView.printError( e.getMessage());
         }  
      }
-    
-    /**
-     * Gets the user option (Encryption/Decryption)
-     * @throws InvalidOptionException When the option is invalid or text contains letters outside of the English alphabet
-     * @throws InvalidCharacterException when the text contains letters outside of the English alphabet
-     */
-    private void getOption()throws InvalidOptionException , InvalidCharacterException, EmptyKey{
-        String choice = vigenereCipherView.getChoiceFromUser();
-        switch (choice) {
-            case "1" -> encryptMessage();
-            case "2" -> decryptMessage();
-            case "3" -> System.exit(0);
-            default -> throw new InvalidOptionException("Invalid option was chosen: Please chose '1', '2' or '3' options");
-        }
-    }
-    /**
-     * Gets inputs(message and keyword) from the view and calls the encryption method of the Model
-     * @throws InvalidCharacterException when the message contains letters outside of the English alphabet
-     * @throws EmptyKey when the chosen key is empty
-     */
-    private void encryptMessage() throws InvalidCharacterException, EmptyKey{
-        String keyword = vigenereCipherView.getKeyFromUser();
-        String message = vigenereCipherView.getMessageFromUser();
-        vigenereCipherModel = new VigenereCipherModel(keyword);
-        Stream<String> messageStream = Stream.of(message.split(" "));
-        vigenereCipherView.outputResult(vigenereCipherModel.getEncryptedMessage(messageStream));
-    }
      
-   /**
-    * Gets inputs(cipher text and keyword) from the view and calls the decryption method of the Model
-    * @throws InvalidCharacterException when the cipher text contains letters outside of the English alphabet
-    * @throws EmptyKey when the chosen key is empty
-    */
-    public void decryptMessage()throws InvalidCharacterException, EmptyKey{
-        String keyword = vigenereCipherView.getKeyFromUser();
-        String cipher = vigenereCipherView.getCipherFromUser();
-        vigenereCipherModel = new VigenereCipherModel(keyword);
-        Stream<String> cipherStream = Stream.of(cipher.split(" "));
-        vigenereCipherView.outputResult(vigenereCipherModel.getDecryptedMessage(cipherStream));
-    }
+
+
     public void encryptMessageAfterClick(String keyword, String message) throws InvalidCharacterException, EmptyKey{
         vigenereCipherModel = new VigenereCipherModel(keyword);
         Stream<String> messageStream = Stream.of(message.split(" "));
-        //vigenereCipherView.outputResult(vigenereCipherModel.getEncryptedMessage(messageStream));
+        JOptionPane.showMessageDialog(vigenereGui, "Encryption result: " + String.join(" ",vigenereCipherModel.getEncryptedMessage(messageStream)
+        ), "Info", JOptionPane.INFORMATION_MESSAGE);
+    }
+    
+    public void decryptMessageAfterClick(String keyword, String cipherText) throws InvalidCharacterException, EmptyKey{
+        vigenereCipherModel = new VigenereCipherModel(keyword);
+        Stream<String> cipherStream = Stream.of(cipherText.split(" "));
+          JOptionPane.showMessageDialog(vigenereGui, "Decryption result: " + String.join(" ",vigenereCipherModel.getDecryptedMessage(cipherStream)
+        ), "Info", JOptionPane.INFORMATION_MESSAGE);
     }
     
 }
     
-
-/**
- * Exception class for objects thrown when an invalid option is chosen
- * @author youssef albali
- * @version 1.0
- */
-
-class InvalidOptionException extends Exception {
-    /**
-     * Non-parameter constructor
-     */
-    public InvalidOptionException() {
-    }
-
-    /**
-     * Exception class constructor
-     *
-     * @param message display message
-     */
-    
-    public InvalidOptionException(String message)  {
-        super(message);
-    }
-    
-}
